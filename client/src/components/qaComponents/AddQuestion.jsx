406f3ad839ab93c5b72f783bb5b7f16d896d1af8
Grant Harris <grantxvx@gmail.com>
2020-09-30 22:37:37 -0500

    Refactor to Modal style for push and merge

--- client/src/components/qaComponents/AddQuestion.jsx
+++ client/src/components/qaComponents/AddQuestion.jsx
@@ -47,35 +47,60 @@ const AddQuestion = (props) => {
     setQuestionModal({...questionModal, [e.target.name]: e.target.value});
   };
 
+  const body = (
+    <div className={classes.paper}>
+      <h2 id="add-question-title">Ask Your Question</h2>
+      <h3 id="add-question-subtitle">About the [PRODUCT NAME]</h3>
+
+      <h4 id="add-question-description">Your Question *</h4>
+      <TextField
+        required
+        fullWidth
+        name="question"
+        variant="outlined"
+        multiline
+        rows={6}
+        onChange={handleChange}
+      />
+
+      <h4 id="add-nickname-description">What is your Nickname? *</h4>
+      <TextField required fullWidth name="nickname" variant="outlined" placeholder="Example: Jackson11!" onChange={handleChange}/>
+      <h5 id="nickname-description-warning"><em>For privacy reasons, do not use your full name or email address.</em></h5>
+
+      <h4 id="add-email-description">Your Email *</h4>
+      <TextField
+        required
+        fullWidth
+        name="email"
+        variant="outlined"
+        placeholder="Why did you like the product or not?" helperText="Incorrect entry."
+        onChange={handleChange}
+      />
+      <h5 id="nickname-description-warning"><em>For authentication reasons, you will not be emailed.</em></h5>
+
+      <Button variant="outlined" type="submit" className={classes.button}>Submit</Button>
+    </div>
+  );
+
+
   return (
     <div>
-      <Button variant="contained" onClick={handleOpen} className={classes.button}>ADD A QUESTION +
+      <Button
+        variant="contained"
+        onClick={handleOpen}
+        className={classes.button}
+      >
+        ADD A QUESTION +
       </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
-          </Button>
-        </DialogActions>
-      </Dialog>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
     </div>
   );
 };
@@ -134,3 +159,33 @@ export default AddQuestion;
 //   </div>
 // );
 // };
+
+
+//DIALOG SYSTEM SCAFFOLDING
+{/* <Button variant="contained" onClick={handleOpen} className={classes.button}>ADD A QUESTION +
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog> */}

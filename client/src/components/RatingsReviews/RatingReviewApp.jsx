96dfeb316264752282e4ac196cfa3203468b7d54
alecbrock <alec.m.brock@gmail.com>
2020-10-01 16:22:18 -0500

    took care of conflicts

--- client/src/components/RatingsReviews/RatingReviewApp.jsx
+++ client/src/components/RatingsReviews/RatingReviewApp.jsx
@@ -4,24 +4,17 @@ import axios from 'axios';
 import ReviewList from './ReviewList.jsx';
 import AddAReview from './AddAReview.jsx';
 import dummyData from '/Users/alecbrock/front-end-capstone/client/dummyData.js';
+
 class RatingReviewApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
       reviewData: false,
       product_id: props.num,
       page: 1,
       bool: false,
       lengthTest: false
     }
-=======
-      dummyData: false,
-      num: 1,
-      id: 0,
-      bool: false
-    };
->>>>>>> master
     //bind any functions here
     this.nextTwo = this.nextTwo.bind(this);
     this.nextConditional = this.nextConditional.bind(this);
@@ -31,17 +24,14 @@ class RatingReviewApp extends Component {
   }
   //Methods here
   getReviews() {
-<<<<<<< HEAD
     axios.get(`http://18.224.37.110/reviews/?product_id=${this.state.product_id}&count=2&page=${this.state.page}`)
       .then((results) => {
         if(!this.state.reviewData) {
-          console.log(results.data.results);
         this.setState({
           reviewData: results.data.results
         })
       } else {
         let holder = [...this.state.reviewData, ...results.data.results];
-        console.log(holder);
         this.setState({
           reviewData: holder
         })
@@ -54,32 +44,14 @@ class RatingReviewApp extends Component {
         console.log(err);
       })
   };
-=======
-    axios.get(`/reviews/${this.state.id}`)
-      .then((reviews) => {
-        this.setState({
-          dummyData: this.props.dummyData
-        });
-      })
-      .catch((error) => {
-        // console.log(error);
-      });
-  }
->>>>>>> master
 
   nextTwo(event) {
     event.preventDefault();
     this.setState({
-<<<<<<< HEAD
       page: this.state.page += 1
     })
     this.getReviews();
   };
-=======
-      num: this.state.num += 2
-    });
-  }
->>>>>>> master
 
 
 
@@ -88,7 +60,6 @@ class RatingReviewApp extends Component {
   //SETUP GET REQUEST ONLY NEXT IN LINE
   //INDEX
   nextConditional() {
-<<<<<<< HEAD
     const { reviewData, page, lengthTest } = this.state,
       next = page + 1;
     axios.get(`http://18.224.37.110/reviews/?product_id=${this.state.product_id}&count=2&page=${next}`)
@@ -101,17 +72,6 @@ class RatingReviewApp extends Component {
         console.log(err);
       })
     };
-=======
-    const { dummyData, num } = this.state;
-    if (dummyData && dummyData.results.length > 2) {
-      if (dummyData.results[num] || dummyData.results[num - 1]) {
-        return <Button variant="outlined" onClick={() => this.nextTwo()}>MORE REVIEWS</Button>;
-      } else {
-        return null;
-      }
-    }
-  }
->>>>>>> master
 
     // if (results.data.results.length > 0) {
     //   console.log(results.data.results.length, 'inside if');
@@ -124,11 +84,11 @@ class RatingReviewApp extends Component {
     if (bool) {
       this.setState({
         bool: false
-      });
+      })
     } else {
       this.setState({
         bool: true
-      });
+      })
     }
   }
 
@@ -136,13 +96,13 @@ class RatingReviewApp extends Component {
     const { reviewData, bool, lengthTest } = this.state;
     return (
       <div>
-        {reviewData ? <ReviewList reviews={reviewData.results} /> : null}
+        {reviewData.length > 0 ? <ReviewList reviews={reviewData} /> : null}
         {lengthTest ? <Button variant="outlined" onClick={(event) => this.nextTwo(event)}>MORE REVIEWS</Button> : null}
         <Button variant="outlined" onClick={() => this.setBool()}>MORE REVIEWS +</Button>
         {bool ? <AddAReview /> : null}
       </div>
-    );
+    )
   }
-}
+};
 
 export default RatingReviewApp;
